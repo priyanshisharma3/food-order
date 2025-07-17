f00d09bffbf762c6d11cf8150c750efdeeb22773
Priyanshi <priyanshi.sharma.3105@gmail.com>
2025-07-18 02:26:42 +0530

    first commit

---
+++ Components.md
@@ -0,0 +1,17 @@
+Components in app- 
+    * Header
+        - Logo
+        - Navbar items
+    * Body
+        - Search bar
+        - Search button
+        - Restaurant cards
+            # Image 
+            # Name of restaurant
+            # Ratings
+            # View more button
+    * Footer
+        - Links
+        - Copyright
+        - Address
+        - Contact
---
+++ app.js
@@ -0,0 +1,55 @@
+import React from "react"
+import { createRoot } from "react-dom/client"
+import './index.css'
+
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="company-logo">
+                <img src="https://short-link.me/13ky1" width={150}/>
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+const RestaurantCard = () => {
+    <h3>Meghana Foods</h3>
+}
+
+const Body = () => {
+    return (
+        <div className="body">
+            <div className="search-bar">
+                <button className="search">Search</button>
+            </div>
+            <div className="rest-contain">
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+            </div>
+        </div>
+    )
+}
+
+const AppLayout = () => {
+    return (
+        <div className="app">
+            <Header /> 
+            <Body/>
+        </div>
+    )
+}
+
+const root = createRoot(document.querySelector('.container'))
+
+root.render(<AppLayout/>)
