aef1d3def1054e75f7a92e5147b65ac2da2280bb
Priyanshi <priyanshi.sharma.3105@gmail.com>
2025-08-02 00:31:17 +0530

    add filter

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,18 +1,25 @@
 import resList from "../../utils/restList";
 import RestaurantCard from "./RestaurantCard";
+import { useState } from "react";
 
 const Body = () => {
+
+    let [listOfRest, filterList] = useState(resList);
+    
     return (
         <div className="body">
             <div className="search-bar">
                 <input type="text" placeholder="Search for any restaurant" className="search-box"/>
                 <button className="search">Search</button>
                 <button className="filter" onClick={() => {
-                    
+                    const setList = listOfRest.filter(
+                        (res) => res.data.avgRating > 4
+                    )
+                    filterList(setList)
                 }}>Top Rated</button>
             </div>
             <div className="rest-contain">
-                {resList.map((restaurant) => (
+                {listOfRest.map((restaurant) => (
                         <RestaurantCard
                         key={restaurant.data.id || index}
                         resList={restaurant}
